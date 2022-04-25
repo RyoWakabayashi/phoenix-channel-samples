c2597a4f8e9bf0eb6aebaa90a193d3304b42dd21
RyoWakabayashi <gfdmkm573@gmail.com>
2022-04-25 18:07:54 +0900

    feat: Add Locust

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # phoenix-channel-samples
 
 PhoenixChannel のサンプルコード
+
+## 負荷試験
+
+チャットサーバー起動
+
+```bash
+cd react_chat
+mix phx.server
+```
+
+別ターミナルで Locust 起動
+
+```bash
+locust -f locustfile.py
+```
---
+++ locustfile.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from __future__ import print_function
+
+import json
+import uuid
+import time
+import gevent
+
+from websocket import create_connection
+import six
+
+from locust import HttpUser, TaskSet, task
+from locust import events
+
+
+class ChatTaskSet(TaskSet):
+    def on_start(self):
+        self.user_id = six.text_type(uuid.uuid4())
+        self.connect()
+        self.join()
+
+    def on_stop(self):
+        self.leave()
+
+    def connect(self):
+        ws = create_connection(
+            'ws://localhost:4000/socket/websocket?token=undefined&vsn=2.0.0')
+        self.ws = ws
+
+    def join(self):
+        body = json.dumps(
+            ["3", "3", "room:lobby", "phx_join", {"user_name": self.user_id}])
+        self.ws.send(body)
+
+    def leave(self):
+        self.ws.close()
+
+    @task
+    def sent(self):
+        start_at = time.time()
+        body = json.dumps(
+            ["3", "4", "room:lobby", "new_msg", {"msg": "hello"}])
+        self.ws.send(body)
+        events.request_success.fire(
+            request_type='WebSocket Sent',
+            name='test/ws/chat',
+            response_time=int((time.time() - start_at) * 1000000),
+            response_length=len(body),
+        )
+        time.sleep(1)
+
+
+class ChatLocust(HttpUser):
+    tasks = [ChatTaskSet]
+    min_wait = 500
+    max_wait = 1000
